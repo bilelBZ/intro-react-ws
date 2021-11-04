c996091357a3227d2bb81c27786266d874fc193a
bilelBZ <bilelbz155@gmail.com>
2021-11-04 16:27:04 +0000

    intro-react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Navb from './Components/Navbar'
+import Formulaire from './Components/Formulaire'
+
+function App() {
+  return (
+    <div class="App">
+     <Navb/>
+     <Formulaire/>
+     <img src=""  />
+     <ul>
+       <li>Learn html</li>
+       <li>learn css</li>
+       <li>Learn React </li>
+     </ul>
+    </div>
+  );
+}
+
+export default App;
